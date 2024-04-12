56d3707214a284a84a4e5d0e2a1491e9a1281ecf
Jan Kubuschok <jk@suchwerk.de>
2024-04-12 14:53:52 +0200

    updated readme

---
+++ src/main/java/org/acme/MyEntityItem.java
@@ -0,0 +1,38 @@
+package org.acme;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+
+/**
+ * Example JPA entity defined as a Panache Entity.
+ * An ID field of Long type is provided, if you want to define your own ID field extends <code>PanacheEntityBase</code> instead.
+ *
+ * This uses the active record pattern, you can also use the repository pattern instead:
+ * .
+ *
+ * Usage (more example on the documentation)
+ *
+ * {@code
+ *     public void doSomething() {
+ *         MyEntity entity1 = new MyEntity();
+ *         entity1.field = "field-1";
+ *         entity1.persist();
+ *
+ *         List<MyEntity> entities = MyEntity.listAll();
+ *     }
+ * }
+ */
+@Entity
+public class MyEntityItem extends PanacheEntity {
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "myentity_id")
+    public MyEntity myEntity;
+
+    public String name;
+}
